792699f37f976484da53b1a748f2873d9bc8e7b0
Luna <lunahayesmy@gmail.com>
2025-08-26 13:55:28 -0400

    workshop 1

---
+++ day-one.py
@@ -0,0 +1,24 @@
+
+int = 1
+float = 2.3 
+
+print(int + float)
+
+print(5*5)
+
+my_list1 = [2, 4, "dog"]
+
+print(my_list1[1])
+print(my_list1[2])
+
+print(len(my_list1))
+
+
+my_str = "The quick brown fox jumps over the lazy dog"
+
+print(my_str)
+print(my_str[4])
+print(my_str[4:9]) #4:9 returns characters 4-8 
+print(my_str[-4:])
+print(my_str[10:13])
+
